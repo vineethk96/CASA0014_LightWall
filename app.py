8fcfebd8522fee9ad7208fb3b69eb1c2de44eb2e
VineethK <vineethk96@users.noreply.github.com>
2024-11-20 19:18:10 +0000

    feat: basic tree functionality is done, but requires a ton of code cleanup. ToDo: create a single Node, and change the colors in a single loop switch out color ints for constants loop through the pixelid when adding to the array add animation ability to show snow falling, or ornaments blinking

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from paho.mqtt import client as mqtt_client
 
 broker = "mqtt.cetools.org"
 port = 1884
-topic = "UCL/student/CASA0014/light/"
+topic = "student/CASA0014/light/"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 file = "./cred.txt"
 username = ""
@@ -31,53 +31,359 @@ MAX_RECONNECT_DELAY = 60
 # LED Node map for the Christmas Tree
 tree = {
     "trunk": [24, 25, 30, 34, 35], 
-    "leaves": [16, 19, 20, 22, 23, 28, 29, 32, 33, 36, 37, 41],
+    "leaves": [12, 15, 16, 19, 20, 22, 23, 27, 28, 29, 32, 33, 38, 39, 42, 43, 47],
     "ornaments": [],
     "star": [26],
-    "snow": [3, 5, 7, 9, 11, 14, 17] # Only half the snow is listed
+    "snow": [1, 4, 6, 9, 18, 36, 41, 44, 49, 52] # Only half the snow is listed
 }
 
-pixID_key = "pixelid"
-R_key = "R"
-G_key = "G"
-B_key = "B"
-W_key = "W"
-bright_key = "brightness"
-method_key = "method"
-allLed_key = "allLEDs"
+clearMsg = {"method": "clear"}
+trunkNode = [
+            {"pixelid":0,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":1,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":2,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":3,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":4,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":5,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":6,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":7,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":8,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":9,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":10,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":11,
+             "R": 150,
+             "G": 75,
+             "B": 0,
+             "W": 0
+            }
+        ]
+leafNode = [
+            {"pixelid":0,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":1,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":2,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":3,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":4,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":5,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":6,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":7,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":8,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":9,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":10,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            },
+            {"pixelid":11,
+             "R": 66,
+             "G": 205,
+             "B": 47,
+             "W": 0
+            }
+        ]
+starNode = [
+            {"pixelid":0,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":1,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":2,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":3,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":4,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":5,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":6,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":7,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":8,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":9,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":10,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            },
+            {"pixelid":11,
+             "R": 255,
+             "G": 255,
+             "B": 0,
+             "W": 0
+            }
+        ]
+snowNode = [
+            {"pixelid":0,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":1,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":2,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":3,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":4,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":5,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":6,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":7,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":8,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":9,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":10,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            },
+            {"pixelid":11,
+             "R": 255,
+             "G": 250,
+             "B": 250,
+             "W": 0
+            }
+        ]
 
-class AbcClass:
-    def __init__(self, in_object):
-        self.obj = in_object
+delay = 0.5
 
-    def dictionary(self):
-        obj = self.obj
-        return dict(a = obj['id'], b = obj['foo'], c = obj['bar'])
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    christmasTree(client)
+    client.loop_stop()
 
-def main():
-
-    christmasTree()
-    # user_input = input("Action:")
-
-    # if(user_input.lower() == "christmas tree"):
-    #     christmasTree()
-    # else:
-    #     print("Unknown Input")    
-
-def christmasTree():
+def christmasTree(client):
     print("Building the Christmas Tree...")
 
-    trunkPixel = {
-        pixID_key:0,
-        R_key:150,
-        G_key:75,
-        B_key:0,
-        W_key:0
-    }
+    # Clear all Lights
+    for i in range(1, 54, 1):
+        newTopic = topic + f"{i}/all/"
+        publish(client, newTopic, clearMsg)
+        time.sleep(delay)
 
-    trunkNode = {
-        allLed_key:[trunkPixel]
-    }
+    # Light up the basic Tree Trunk
+    for i in range(len(tree["trunk"])):
+        newTopic = topic + f"{tree['trunk'][i]}/all/"
+        newMsg = {"allLEDs": trunkNode}
+        publish(client, newTopic, newMsg)
+        time.sleep(delay)
+
+    # Light up the basic Tree Leaves
+    for i in range(len(tree["leaves"])):
+        newTopic = topic + f"{tree["leaves"][i]}/all/"
+        newMsg = {"allLEDs": leafNode}
+        publish(client, newTopic, newMsg)
+        time.sleep(delay)
+
+    # Light up the snow
+    for i in range(len(tree["snow"])):
+        newTopic = topic + f"{tree["snow"][i]}/all/"
+        newMsg = {"allLEDs": snowNode}
+        publish(client, newTopic, newMsg)
+        time.sleep(delay)
+
+    # Light up the Star
+    newTopic = topic + f"{tree['star'][0]}/all/"
+    newMsg = {"allLEDs": starNode}
+    publish(client, newTopic, newMsg)
+    time.sleep(delay)
+
+
+    while True:
+        time.sleep(delay)
+
+        
 
     for i in range(12):
         trunkPixel.update({pixID_key, i})
@@ -97,26 +403,17 @@ def christmasTree():
     #     # Update every 500ms
     #     time.sleep(0.5)
 
-        
-
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
-    # For paho-mqtt 2.0.0, you need to add the properties parameter.
-    # def on_connect(client, userdata, flags, rc, properties):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
-    # Set Connecting Client ID
+
     client = mqtt_client.Client(client_id)
-
-    # For paho-mqtt 2.0.0, you need to set callback_api_version.
-    client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
-
     client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
-    client.on_disconnect = on_disconnect
     return client
 
 def on_disconnect(client, userdata, rc):
@@ -138,12 +435,22 @@ def on_disconnect(client, userdata, rc):
         reconnect_count += 1
     logging.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)
 
-if __name__ == "__main__":
+def publish(client, newTopic, newMsg):
+    result = client.publish(newTopic, f"{newMsg}")
+    status = result[0]
+    if status == 0:
+        print(f"Send `{newMsg}` to topic `{newTopic}`")
+    else:
+        print(f"Failed to send message to topic {newTopic}")
+
+if __name__ == '__main__':
     print("Starting LightWall App...")
 
     # Pull MQTT username and password
     doc = open(file, "r")
-    username = doc.readline()
-    password = doc.readline()
+    username = doc.readline().strip()
+    password = doc.readline().strip()
+    print("username: " + username)
+    print("password: " + password)
     doc.close()
-    main()
+    run()
